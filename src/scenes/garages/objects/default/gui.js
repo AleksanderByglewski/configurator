72862cc544994597d2134f75d2b56b7c2b808a35
Aleksander Byglewski <alexbyglewski@icloud.com>
2023-08-30 06:39:25 +0200

    Deleted a file g

--- src/scenes/garages/objects/default/gui.js
+++ src/scenes/garages/objects/default/gui.js
@@ -5,9 +5,9 @@ import * as THREE from 'three';
 import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
 import { Generic, genericGui, genericState, genericObject, genericDisplay, genericController } from '../../base.js'
 import { PlanetGui, PlanetObject, Planet, System } from '../introduction.js'
-import { CubeObject,UconfigObject,WallGarageObject, genericGarageObject } from '../default/object'
+import { CubeObject,UconfigObject,WallGarageObject, genericGarageObject } from './object'
 
-import {UconfigController,CubeController,WallGarageController,groupGenericGarageController,genericGarageController} from '../default/controller'
+import {UconfigController,CubeController,WallGarageController,groupGenericGarageController,genericGarageController} from './controller'
 
 class UconfigInvisibleGui extends genericGui {
     constructor() {
